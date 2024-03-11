1e5bc634017a6f90513279b5ea87c5a30fddd118
Tyler Remazki <tremazki@qaconsultants.com>
2024-03-11 15:19:16 -0400

    Added missing param dependency for junit5 and added a parameterized test as an example

--- src/test/java/org/example/selenium/google/GoogleSearchTest.java
+++ src/test/java/org/example/selenium/google/GoogleSearchTest.java
@@ -3,15 +3,17 @@ package org.example.selenium.google;
 import org.example.junit.SeleniumTest;
 import org.example.model.pages.google.GoogleHomePage;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class GoogleSearchTest extends SeleniumTest {
 
-    @Test
     @DisplayName("Test Google Search Functionality")
-    void testGoogleSearch() {
+    @ParameterizedTest
+    @ValueSource(strings = {"Test", "Automation"})
+    void testGoogleSearch(String searchTerm) {
         GoogleHomePage home = new GoogleHomePage(driver).get();
-        home.enterTextAndSubmit("Test");
+        home.enterTextAndSubmit(searchTerm);
         home.validateUrlContains("search");
     }
 
